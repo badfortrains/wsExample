b142d485b0fefbd044c3d975b74ce6379eae0dbc
Sean Purcell <s3purcell@gmail.com>
2015-04-04 15:43:32 -0700

    test for firebase, socket-io.  Modified socket.io / firebase includes for react-native

---
+++ index.ios.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var io = require("./socket.io-client");
+//Must specifiy 'jsonp: false' since react native doesn't provide the dom
+//and thus wouldn't support creating an iframe/script tag
+var socket = io('http://wupnp.com/controller',{jsonp: false});
+  socket.on('connect', function(){ console.log("connect") });
+  socket.on('event', function(data){ console.log(event) });
+  socket.on('disconnect', function(){});
+
+var firebase = require("./firebase-debug")
+// Get a reference to our posts
+var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+
+var wsExample = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+Control+Z for dev menu
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('wsExample', () => wsExample);
